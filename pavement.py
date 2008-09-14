9536a0936f0ba7e593d24b0ec4ff296071380ce1
Matthew R. Scott <gldnspud@gmail.com>
2008-09-14 14:07:26 -0700

    Wrap HTML doc generation between cog/uncog
    Signed-off-by: Matthew R. Scott <gldnspud@gmail.com>

--- pavement.py
+++ pavement.py
@@ -139,13 +139,24 @@ if 'options' in locals():
 
     if paver.doctools.has_sphinx:
         @task
+        @needs('paver.doctools.uncog')
         @needs('paver.doctools.html')
+        @needs('paver.doctools.cog')
+        def html():
+            pass
+
+        @task
+        @needs('paver.doctools.uncog')
+        @needs('paver.doctools.html')
+        @needs('paver.doctools.cog')
         def openhtml():
             index_file = path('doc/build/html/index.html')
             sh('open ' + index_file)
 
         @task
+        @needs('paver.doctools.uncog')
         @needs('paver.doctools.html')
+        @needs('paver.doctools.cog')
         @cmdopts([("username=", "u", "Username for remote server"),
                   ("server=", "s", "Server to publish to"),
                   ("path=", "p", "Path to publish to")])
