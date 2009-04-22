840a141d3372afd77b3b3125b551f5dc0d5f1f42
Matthew R. Scott <gldnspud@gmail.com>
2009-04-21 20:05:16 -0700

    Depend on proper SchevoDurus version

--- pavement.py
+++ pavement.py
@@ -94,7 +94,7 @@ setup(
     install_requires=[
         'Louie >= 1.1',
         'PasteScript >= 1.7.3',
-        'SchevoDurus >= 3.1a2',
+        'SchevoDurus >= 3.1-dev',
     ],
     tests_require=['nose >= 0.10.4'],
     test_suite='nose.collector',
