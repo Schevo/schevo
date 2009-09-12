ab49d99efce190a91cc47840c58dc17943ce8edd
Matthew R. Scott <gldnspud@gmail.com>
2009-09-11 21:57:19 -0700

    Use latest SchevoDurus, which uses Durus 3.9.

--- pavement.py
+++ pavement.py
@@ -105,7 +105,7 @@ else:
         install_requires=[
             'Louie >= 1.1',
             'PasteScript >= 1.7.3',
-            'SchevoDurus == dev, >= 3.1b1dev-20090502',
+            'SchevoDurus == dev, >= 3.1b1dev-20090911',
         ],
         tests_require=['nose >= 0.10.4'],
         test_suite='nose.collector',
