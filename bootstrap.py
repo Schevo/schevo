4a0114da8aba4ef0e4f58e82d016b9aa0c1874e4
Matthew R. Scott <gldnspud@gmail.com>
2009-09-16 15:23:16 -0700

    Prevent sys.args parsing upon import.

--- bootstrap.py
+++ bootstrap.py
@@ -20,65 +20,67 @@ use the -c option to specify an alternate configuration file.
 $Id$
 """
 
-import os, shutil, sys, tempfile, urllib2
+# Wrap so it doesn't try to parse args upon import.
+if __name__ == '__main__':
+    import os, shutil, sys, tempfile, urllib2
 
-tmpeggs = tempfile.mkdtemp()
+    tmpeggs = tempfile.mkdtemp()
 
-is_jython = sys.platform.startswith('java')
+    is_jython = sys.platform.startswith('java')
 
-try:
-    import pkg_resources
-except ImportError:
-    ez = {}
-    exec urllib2.urlopen('http://peak.telecommunity.com/dist/ez_setup.py'
-                         ).read() in ez
-    ez['use_setuptools'](to_dir=tmpeggs, download_delay=0)
+    try:
+        import pkg_resources
+    except ImportError:
+        ez = {}
+        exec urllib2.urlopen('http://peak.telecommunity.com/dist/ez_setup.py'
+                             ).read() in ez
+        ez['use_setuptools'](to_dir=tmpeggs, download_delay=0)
 
-    import pkg_resources
+        import pkg_resources
 
-if sys.platform == 'win32':
-    def quote(c):
-        if ' ' in c:
-            return '"%s"' % c # work around spawn lamosity on windows
-        else:
+    if sys.platform == 'win32':
+        def quote(c):
+            if ' ' in c:
+                return '"%s"' % c # work around spawn lamosity on windows
+            else:
+                return c
+    else:
+        def quote (c):
             return c
-else:
-    def quote (c):
-        return c
 
-cmd = 'from setuptools.command.easy_install import main; main()'
-ws  = pkg_resources.working_set
+    cmd = 'from setuptools.command.easy_install import main; main()'
+    ws  = pkg_resources.working_set
 
-if len(sys.argv) > 2 and sys.argv[1] == '--version':
-    VERSION = ' == %s' % sys.argv[2]
-    args = sys.argv[3:] + ['bootstrap']
-else:
-    VERSION = ''
-    args = sys.argv[1:] + ['bootstrap']
+    if len(sys.argv) > 2 and sys.argv[1] == '--version':
+        VERSION = ' == %s' % sys.argv[2]
+        args = sys.argv[3:] + ['bootstrap']
+    else:
+        VERSION = ''
+        args = sys.argv[1:] + ['bootstrap']
 
-if is_jython:
-    import subprocess
+    if is_jython:
+        import subprocess
 
-    assert subprocess.Popen([sys.executable] + ['-c', quote(cmd), '-mqNxd',
-           quote(tmpeggs), 'zc.buildout' + VERSION],
-           env=dict(os.environ,
-               PYTHONPATH=
-               ws.find(pkg_resources.Requirement.parse('setuptools')).location
-               ),
-           ).wait() == 0
+        assert subprocess.Popen([sys.executable] + ['-c', quote(cmd), '-mqNxd',
+               quote(tmpeggs), 'zc.buildout' + VERSION],
+               env=dict(os.environ,
+                   PYTHONPATH=
+                   ws.find(pkg_resources.Requirement.parse('setuptools')).location
+                   ),
+               ).wait() == 0
 
-else:
-    assert os.spawnle(
-        os.P_WAIT, sys.executable, quote (sys.executable),
-        '-c', quote (cmd), '-mqNxd', quote (tmpeggs), 'zc.buildout' + VERSION,
-        dict(os.environ,
-            PYTHONPATH=
-            ws.find(pkg_resources.Requirement.parse('setuptools')).location
-            ),
-        ) == 0
+    else:
+        assert os.spawnle(
+            os.P_WAIT, sys.executable, quote (sys.executable),
+            '-c', quote (cmd), '-mqNxd', quote (tmpeggs), 'zc.buildout' + VERSION,
+            dict(os.environ,
+                PYTHONPATH=
+                ws.find(pkg_resources.Requirement.parse('setuptools')).location
+                ),
+            ) == 0
 
-ws.add_entry(tmpeggs)
-ws.require('zc.buildout' + VERSION)
-import zc.buildout.buildout
-zc.buildout.buildout.main(args)
-shutil.rmtree(tmpeggs)
+    ws.add_entry(tmpeggs)
+    ws.require('zc.buildout' + VERSION)
+    import zc.buildout.buildout
+    zc.buildout.buildout.main(args)
+    shutil.rmtree(tmpeggs)
